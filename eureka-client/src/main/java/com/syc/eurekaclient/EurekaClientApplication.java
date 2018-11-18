192c3a8217d41f1a56f3faced17a6342517b8a5e
syc <398399363@qq.com>
2018-11-18 22:13:14 +0800

    project create

---
+++ eureka-client/src/main/java/com/syc/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,35 @@
+package com.syc.eurekaclient;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class EurekaClientApplication {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/")
+    public String home(){
+        List<String> services = discoveryClient.getServices();
+        services.forEach(System.out::println);
+
+        return "hello world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
