5fd13d4001f61c837aa758048d081e3d20626c2d
syc <398399363@qq.com>
2018-12-21 14:23:50 +0800

    new demo

--- feign-consumer/src/main/java/com/syc/feignconsumer/HelloService.java
+++ feign-consumer/src/main/java/com/syc/feignconsumer/HelloService.java
@@ -13,12 +13,29 @@ public interface HelloService {
     String hello();
 
 
+    /**
+     * param 数据绑定测试
+     *
+     * @param name
+     * @return
+     */
     @RequestMapping("/hello1")
     String hello1(@RequestParam String name);
 
+    /**
+     * header 数据绑定测试
+     * @param name
+     * @param age
+     * @return
+     */
     @RequestMapping("/hello2")
     User hello2(@RequestHeader String name, @RequestHeader Integer age);
 
+    /**
+     * body 数据绑定测试
+     * @param user
+     * @return
+     */
     @RequestMapping("/hello3")
     String hello3(@RequestBody User user);
 }
